edddf5da73b21237b8da5b0ff11c1e2d9f7f6189
Vasyl <vasja112@gmail.com>
2025-07-23 18:17:07 +0300

    Мова сайту береться із значення параметра langDetected

--- js/language-switcher.js
+++ js/language-switcher.js
@@ -1,26 +1,29 @@
 // Для переключалки сайту на іншу мову
 const langSwitcher = document.getElementById("switchLang");
 
-// Перевірка мови браузера при першому заході (в локальному сховищі немає параметра langDetected)
-// Якщо параметр langDetected є, то не залежно від значення його, з якою мовою(url) заходиться на сайт, та мова і буде відображатись.
+/* Перевірка мови браузера при першому заході (в локальному сховищі немає параметра langDetected)
+Якщо параметр langDetected немає, то підгружається мова сайту відносно умови: 'uk' || 'ru' => 'uk' версія сайту
+Всі інші мови => 'en' версія сайту
+Якщо параметр langDetected є, то залежно від значення його, підгрузиться відповідна версія сайту. */
 (function detectLangOnFirstVisit() {
-  const isLangDetected = localStorage.getItem('langDetected');
-  const userLang = navigator.language || navigator.userLanguage;
+  const storedLang = localStorage.getItem('langDetected');
+  const currentLang = window.location.href.includes("-en") ? "en" : "uk";
 
-  if (!isLangDetected) {
-    let targetUrl;
-
-    if (userLang.startsWith('uk') || userLang.startsWith('ru')) {
-      // Визначена українська або російська — ведемо на uk-версію
-      targetUrl = 'index.html';
-      localStorage.setItem('langDetected', 'ua');
-    } else {
-      // Всі інші — англійська версія
-      targetUrl = 'index-en.html';
-      localStorage.setItem('langDetected', 'en');
+  // Якщо мова збережена в локальному сховищі, але поточна не відповідає їй
+  if (storedLang && storedLang !== currentLang) {
+    const targetUrl = storedLang === "en" ? "index-en.html" : "index.html";
+    if (!window.location.href.includes(targetUrl)) {
+      window.location.href = targetUrl;
     }
+    return;
+  }
 
-    // Якщо поточна сторінка не відповідає targetUrl — перенаправляємо
+  // Якщо мова ще не визначена(перший вхід), то визначити за navigator.language
+  if (!storedLang) {
+    const browserLang = navigator.language.startsWith("uk") ? "uk" : "en";
+    localStorage.setItem("langDetected", browserLang);
+
+    const targetUrl = browserLang === "en" ? "index-en.html" : "index.html";
     if (!window.location.href.includes(targetUrl)) {
       window.location.href = targetUrl;
     }
